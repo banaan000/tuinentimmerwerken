44fdd7d37bf4bb5fcd3cbe8ef6e711e1c45db405
banaan000 <banaan000@gmail.com>
2025-02-17 20:48:32 +0100

    fixes

--- scripts.js
+++ scripts.js
@@ -1,21 +1,3 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dropbtn = document.querySelector(".dropbtn");
-    const dropdownContent = document.querySelector(".dropdown-content");
-
-    dropbtn.addEventListener("click", function(event) {
-        event.preventDefault();
-        dropdownContent.classList.toggle("show");
-    });
-
-    // Close the dropdown if the user clicks outside of it
-    window.addEventListener("click", function(event) {
-        if (!event.target.matches('.dropbtn')) {
-            if (dropdownContent.classList.contains('show')) {
-                dropdownContent.classList.remove('show');
-            }
-        }
-    });
-});
 
 // Carousel Functionality
 const carouselInner = document.querySelector('.carousel-inner');
@@ -84,30 +66,6 @@ dots.forEach((dot, index) => {
     dot.addEventListener('click', () => changeTestimonial(index));
 });
 
-// document.addEventListener('DOMContentLoaded', function() {
-//     var encodedPhone = 'MDYgNDIyIDY1IDQ2MA=='; 
-//     var encodedAddress = 'SnVsaWFuYWxhYW4gNzAsIDM4NDMgQ0MgSGFyZGVyd2lqaw=='; 
-//     var encodedUser = 'YmF1a2pldmFuZWx0ZW4='; 
-//     var encodedDomain = 'Z21haWwuY29t'; 
-
-//     var phone = atob(encodedPhone);
-//     var address = atob(encodedAddress);
-//     var user = atob(encodedUser);
-//     var domain = atob(encodedDomain);
-
-//     var contact = user + '@' + domain;
-
-//     var phoneElement = document.getElementById('phone-code');
-//     phoneElement.textContent = phone; 
-//     phoneElement.removeAttribute('href'); 
-
-//     var addressElement = document.getElementById('address-code');
-//     addressElement.innerHTML = address.replace(/,/g, '<br>'); 
-
-//     var contactElement = document.getElementById('contact-code');
-//     contactElement.textContent = contact; 
-//     contactElement.removeAttribute('href'); 
-
-//     var iconLink = document.getElementById('icon-code');
-//     iconLink.href = 'mailto:' + contact;
-// });
+document.getElementById("hamburger").addEventListener("click", function() {
+    document.getElementById("nav-menu").classList.toggle("active");
+});
